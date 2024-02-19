00ef5058250a7b4a5646038c836009689b6ca1e4
JP <joel720@gmail.com>
2024-02-19 15:10:18 -0600

    Housekeeping
    Update packages, additional tailwind configs, and add arches logo.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,8 +9,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        accent: "#ffbc0d",
+      }
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
 export default config;
