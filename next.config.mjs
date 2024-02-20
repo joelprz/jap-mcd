e1821c4e4828bea66d46a41345a14f8d39db71ec
JP <joel720@gmail.com>
2024-02-20 09:55:10 -0600

    Add config to display images
    Images not displaying when deployed to Vercel. 1st fix attempt.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'jap-mcd.vercel.app',
+        ],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ]
+    },
+};
 
 export default nextConfig;
