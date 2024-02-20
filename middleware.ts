cfb07dbb5c3f9d1642f9ccbc6d42bb5c1e5e4d40
JP <joel720@gmail.com>
2024-02-20 10:10:27 -0600

    Remove image matcher from middleware
    Images not displaying when deployed to Vercel. 4th fix attempt.

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
  
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/((?!api|_next/products|_next/image|.*\\.jpeg$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
