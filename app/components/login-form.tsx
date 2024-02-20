ddee687ea5949976a1d593c71b0c36e35f9bfbb5
JP <joel720@gmail.com>
2024-02-20 09:37:47 -0600

    Change login input styles to McD

--- app/components/login-form.tsx
+++ app/components/login-form.tsx
@@ -18,7 +18,7 @@ export default function LoginForm() {
             </label>
             <div className="mt-2">
                 <input
-                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
                     id="email"
                     type="email"
                     name="email"
@@ -36,7 +36,7 @@ export default function LoginForm() {
             </label>
             <div className="mt-2 relative">
                 <input
-                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
                     id="password"
                     type="password"
                     name="password"
