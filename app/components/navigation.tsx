659e4f1f5307c8375e63597a6343075e5e7a056e
JP <joel720@gmail.com>
2024-02-21 10:58:39 -0600

    Hide nav items when not authenticated.
    Use session status to determine if cart and logout buttons should be displayed.

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import {auth} from "@/auth";
 import CartLink from "./cartLink";
 import arches from "../../public/arches-logo.svg";
 import LogoutButton from "./logoutBtn";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <div className="border-b border-gray-200 bg-white">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -28,9 +30,10 @@ export default function Navigation() {
           </div>
           <div className="flex items-center px-2 lg:px-0">
             <div className="flex-shrink-0 flex space-x-2">
-              <CartLink path="/order/cart" />
-              {/*TODO Hide logout button when user is not authenticated*/}
-              <LogoutButton />
+              {session && session.user &&
+                  <><CartLink path="/order/cart" />
+                  <LogoutButton /></>
+              }
             </div>
           </div>
         </div>
